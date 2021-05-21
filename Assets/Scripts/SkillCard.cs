6961251612f33d8ab0a31bd0048a98207fa4c651
JasonReff <jmr8382@rit.edu>
2021-05-21 19:36:12 -0400

    rename skillcard1 to skillcard. refactor skillcard to use public character data and activate method.

---
+++ Assets/Scripts/SkillCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SkillCard : MonoBehaviour
+{
+    public CombatSystem combatSystem;
+    public int skillNumber;
+    public Character activePlayer;
+    public Skill skill;
+    public Text nameText;
+    public Text SPCost;
+    public Text description;
+
+    private void Start()
+    {
+        StartCoroutine(SkillUpdate());
+    }
+    IEnumerator SkillUpdate()
+    {
+        while (true)
+        {
+            activePlayer = combatSystem.activePlayer;
+            skill = activePlayer.skills[skillNumber];
+            nameText.text = skill.ToString();
+            SPCost.text = skill.SPCost.ToString();
+            description.text = skill.skillDescription;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+    void OnMouseDown()
+    {
+        skill.Activate();
+    }
+}
