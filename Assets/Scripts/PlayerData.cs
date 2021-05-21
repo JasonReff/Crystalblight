4f5667b0beb3d285961331e97380f15d1c39cd47
JasonReff <jmr8382@rit.edu>
2021-05-21 19:08:44 -0400

    add Don'tDestroyOnLoad to PlayerData

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class PlayerData : MonoBehaviour
 {
     public List<Character> characters;
+    private static PlayerData instance = null;
 
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
 
 }
