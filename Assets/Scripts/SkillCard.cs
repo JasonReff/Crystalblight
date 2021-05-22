3b7143228b8a2e5c850dbd8c80f021f5a9649cf7
JasonReff <jmr8382@rit.edu>
2021-05-22 12:30:11 -0400

    create skillcard setactive coroutine.
    while there is an active player, the skillcard appears. when there is no active player, the skill card is set to inactive.

--- Assets/Scripts/SkillCard.cs
+++ Assets/Scripts/SkillCard.cs
@@ -12,25 +12,47 @@ public class SkillCard : MonoBehaviour
     public Character activePlayer;
     public Skill skill;
     public Text nameText;
+    public Text damage;
     public Text SPCost;
     public Text description;
 
     private void Start()
     {
         StartCoroutine(SkillUpdate());
+        StartCoroutine(SkillCardAppear());
     }
     IEnumerator SkillUpdate()
     {
         while (true)
         {
-            activePlayer = combatSystem.activePlayer;
-            skill = activePlayer.skills[skillNumber];
-            nameText.text = skill.ToString();
-            SPCost.text = skill.SPCost.ToString();
-            description.text = skill.skillDescription;
+            SkillCardUpdate();
             yield return new WaitForSeconds(0.2f);
         }
     }
+
+    void SkillCardUpdate()
+    {
+        activePlayer = combatSystem.activePlayer;
+        skill = activePlayer.skills[skillNumber];
+        nameText.text = skill.ToString();
+        damage.text = skill.baseDamage.ToString();
+        SPCost.text = skill.SPCost.ToString();
+        description.text = skill.skillDescription;
+    }
+
+    IEnumerator SkillCardAppear()
+    {
+        while (activePlayer = null)
+        {
+            gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+        }
+        while (activePlayer != null)
+        {
+            gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+        }
+    } 
     void OnMouseDown()
     {
         if (skill.targetingType == Skill.TargetingType.Untargeted)
