c5f4106dca04ed493777817781eecbf4fbaf3627
JasonReff <jmr8382@rit.edu>
2021-05-23 14:00:38 -0400

    delete outdated enemy skills script.
    get rid of calls to old skill scripts in player turn.

--- Assets/Scripts/PlayerTurn.cs
+++ Assets/Scripts/PlayerTurn.cs
@@ -15,40 +15,9 @@ public class PlayerTurn : MonoBehaviour
         {
             if (PlayerPrefs.GetString("P" + p + "StartUp") != "null")
             {
-                AutoAttack(p);
                 StartCoroutine(AttackDelay());
             }
         }
-    }
-
-    public void AutoAttack(int player)
-    {
-        string skill = PlayerPrefs.GetString("P" + player + "StartUp");
-        switch (PlayerPrefs.GetString(skill + "-Targeting"))
-        {
-            case "SingleTarget":
-                SingleTargetSkills singleTargetSkills = GameObject.Find("E1").GetComponent<SingleTargetSkills>();
-                singleTargetSkills.Invoke(skill, 0f);
-                break;
-            case "FriendlyTarget":
-                FriendlyTargetSkills friendlyTargetSkills = GameObject.Find("skillcard-1").GetComponent<FriendlyTargetSkills>();
-                friendlyTargetSkills.Invoke(skill, 0f);
-                break;
-            case "FriendlyTargetOther":
-                FriendlyTargetOtherSkills friendlyTargetOtherSkills = GameObject.Find("skillcard-1").GetComponent<FriendlyTargetOtherSkills>();
-                friendlyTargetOtherSkills.Invoke(skill, 0f);
-                break;
-            case "Untargeted":
-                UntargetedSkills untargetedSkills = GameObject.Find("skillcard-1").GetComponent<UntargetedSkills>();
-                untargetedSkills.Invoke(skill, 0f);
-                break;
-            case "EnemyTile": 
-            case "EnemyRow":
-                EnemyTileSkills enemyTileSkills = GameObject.Find("E-Block-1").GetComponent<EnemyTileSkills>();
-                enemyTileSkills.Invoke(skill, 0f);
-                break;
-        }
-        
     }
 
     IEnumerator AttackDelay()
