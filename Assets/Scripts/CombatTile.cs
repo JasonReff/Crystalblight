9df4cacbc977926796ead782fd351072ba4d2bb7
JasonReff <jmr8382@rit.edu>
2021-05-23 12:25:28 -0400

    Add adjacentTiles method to combatTile script

--- Assets/Scripts/CombatTile.cs
+++ Assets/Scripts/CombatTile.cs
@@ -6,6 +6,7 @@ public class CombatTile : MonoBehaviour
 {
     public int columnNumber;
     public int rowNumber;
+    public bool movable;
 
     public CombatTile(int column, int row)
     {
@@ -13,5 +14,13 @@ public class CombatTile : MonoBehaviour
         rowNumber = row;
     }
 
-    
+    public static List<CombatTile> AdjacentTiles(CombatTileSet tileSet, CombatTile location)
+    {
+        CombatTile up = tileSet.tiles.Find(x => x.columnNumber == location.columnNumber && x.rowNumber == location.rowNumber + 1);
+        CombatTile left = tileSet.tiles.Find(x => x.columnNumber == location.columnNumber - 1 && x.rowNumber == location.rowNumber);
+        CombatTile right = tileSet.tiles.Find(x => x.columnNumber == location.columnNumber + 1 && x.rowNumber == location.rowNumber);
+        CombatTile down = tileSet.tiles.Find(x => x.columnNumber == location.columnNumber + 1 && x.rowNumber == location.rowNumber);
+        List<CombatTile> adjacentTiles = new List<CombatTile> { up, left, down, right };
+        return adjacentTiles;
+    }
 }
