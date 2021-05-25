beb415277d9fb6b48c98a9773ad121fd741db9f7
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-05-25 16:32:38 -0400

    ReadPref update
    This one should have it?

--- Assets/Scripts/ReadPref.cs
+++ Assets/Scripts/ReadPref.cs
@@ -14,11 +14,42 @@ public class ReadPref : MonoBehaviour
                 {
                     return values[1];
                 }
-                return "null";
+                //return "null";
             }
             return "null";
         }
     }
+
+    public static string FindFromCSV_PLUS_C(string dataFile, string RowName, string CollumnName)
+    {
+        using (var reader = new StreamReader(dataFile))
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(',');
+            var names = values;
+            int collumn = 0;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == CollumnName)
+                {
+                    collumn = i;
+                }
+            }
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                values = line.Split(',');
+                if (values[0] == RowName)
+                {
+
+                    return values[collumn];
+                }
+                //return "null";
+            }
+            return "null";
+        }
+    }
+
     public static void GetStringFromIndex(int index)
     {
         using (var reader = new StreamReader("pref.csv"))
