17b5a516868b071546916bb2940a881f2bcab46c
JasonReff <jmr8382@rit.edu>
2021-05-26 01:49:11 -0400

    map creation methods
    methods for filling map with new maptile objects. method for filling tilemap with map object data.

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Map : MonoBehaviour
 {
     public List<MapTile> mapTiles;
+    public Tilemap tilemap;
+    public Tile emptyTile;
+    public Tile homeTile;
+    public Tile characterTile;
+    public Tile combatTile;
+    public Tile shopTile;
+    public Tile gamblerTile;
+    public Tile essenceTile;
 
     public Map()
     {
@@ -16,7 +25,59 @@ public class Map : MonoBehaviour
         {
             for (int y = lowermostTile; y <= uppermostTile; y++)
             {
-                MapTile mapTile = new MapTile(x, y);
+                MapTile mapTile = MapTile.RandomMapTile(x, y);
+            }
+        }
+        foreach (MapTile mapTile in mapTiles)
+        {
+            LeftMapTilesEmpty(this, mapTile);
+        }
+    }
+
+
+
+    public void LeftMapTilesEmpty(Map map, MapTile mapTile)
+    {
+        List<MapTile> leftTiles = mapTile.LeftTiles(map, mapTile);
+        if (leftTiles.Find(x => x.encounter != null) == null)
+        {
+            mapTile.encounter = null;
+        }
+    }
+
+    public void FillFixedEncounters(Map map)
+    {
+        foreach(MapTile mapTile in map.mapTiles)
+        {
+            Vector3Int tileMapCoordinates = new Vector3Int(mapTile.mapCoordinates[0], mapTile.mapCoordinates[1], 0);
+            switch (mapTile.encounter.encounterType)
+            {
+                case Encounter.EncounterType.Home:
+                    tilemap.SetTile(tileMapCoordinates, homeTile);
+                    break;
+                case Encounter.EncounterType.Character:
+                    tilemap.SetTile(tileMapCoordinates, characterTile);
+                    break;
+            }
+        }
+    }
+
+    public void CreateHomeTile(Map map)
+    {
+        int[] homeCoordinates = new int[] { -10, 0 };
+        MapTile home = map.mapTiles.Find(x => x.mapCoordinates == homeCoordinates);
+        home.encounter.encounterType = Encounter.EncounterType.Home;
+    }
+
+    public void FillVariableEncounters(Map map)
+    {
+        foreach (MapTile mapTile in map.mapTiles)
+        {
+            switch (mapTile.encounter.encounterType)
+            {
+                case Encounter.EncounterType.Combat:
+                    break;
+
             }
         }
     }
