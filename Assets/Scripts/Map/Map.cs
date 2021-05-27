e66401f77bb3d45cc1e7d7d672fff3d55c46e130
JasonReff <jmr8382@rit.edu>
2021-05-27 00:05:03 -0400

    add serializable to map and maptile classes

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+[Serializable]
 public class Map : MonoBehaviour
 {
     public List<MapTile> mapTiles;
